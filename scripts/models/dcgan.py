82b94507770e366ee52f1b3cf5f71fd10892ef50
hca <s7hialtu@uni-bonn.de>
2021-06-11 00:27:57 +0200

    fid is added early stopping is confusing

--- scripts/models/dcgan.py
+++ scripts/models/dcgan.py
@@ -23,7 +23,6 @@ class GAN(pl.LightningModule):
     def __init__(self,
                  img_dim=(3, 32, 64),
                  learning_rate=0.0002,
-                 latent_dim=100,
                  betas=(0.5, 0.999),
                  discriminator_params=None,
                  generator_params=None,
@@ -39,6 +38,13 @@ class GAN(pl.LightningModule):
         self.discriminator = self.get_discriminator(discriminator_params)
         self.criterion = self.get_criterion()
 
+        # ingerating FID score
+        self._n_samples = 1024
+        self._act_real = []
+        self._act_samples = 512
+        self.gen_input = []
+        self.imgs_real = None
+
     def get_criterion(self):
         return nn.BCELoss()
 
@@ -46,11 +52,11 @@ class GAN(pl.LightningModule):
         generator_params = generator_params if generator_params else {}
 
         if self.hparams.gen_model == "resnet":
-            generator = ResNetGenerator(self.hparams.img_dim,
-                                        self.hparams.latent_dim, **generator_params)
+            generator = ResNetGenerator(
+                self.hparams.img_dim, **generator_params)
         elif self.hparams.gen_model == "basic":
-            generator = BasicGenerator(self.hparams.img_dim,
-                                       self.hparams.latent_dim, **generator_params)
+            generator = BasicGenerator(
+                self.hparams.img_dim, **generator_params)
             generator.apply(weights_init_normal)
         elif self.hparams.gen_model == "unet":
             generator = UnetGenerator(
@@ -75,10 +81,13 @@ class GAN(pl.LightningModule):
     def configure_optimizers(self):
         lr = self.hparams.learning_rate
         betas = self.hparams.betas
+
+        # optimizers
         opt_disc = th.optim.Adam(
             self.discriminator.parameters(), lr=lr, betas=betas)
         opt_gen = th.optim.Adam(
             self.generator.parameters(), lr=lr, betas=betas)
+
         return [opt_disc, opt_gen], []
 
     def forward(self, tensor: th.Tensor) -> th.Tensor:
@@ -86,6 +95,26 @@ class GAN(pl.LightningModule):
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         real, fake = batch
+
+        # append real image act and fake image
+        if len(self._act_real) < self._n_samples:
+            act = np.squeeze(vgg16_get_activation_maps(
+                real, layer_idx=33, device="cpu", normalize_range=(-1, 1)).numpy())
+            self._act_real = np.concatenate(
+                (self._act_real, act), axis=0) if len(self._act_real) > 0 else act
+
+        # append fake images or noise vectors
+        if len(self.gen_input) < self._n_samples:
+            if self.hparams.gen_model in ["unet", "refiner"]:
+                self.gen_input = th.cat((self.gen_input, fake), dim=0) if len(
+                    self.gen_input) > 0 else fake
+            else:
+                self.gen_input = th.normal(
+                    0, 1, (self._n_samples, self.generator.latent_dim), device=self.device)
+
+        # store the first batch for callback
+        if self.imgs_real is None:
+            self.imgs_real = real
 
         result = None
 
@@ -108,10 +137,11 @@ class GAN(pl.LightningModule):
                 gp_loss = 0.5 * self.hparams.alpha * th.mean(slopes)
 
             # Train with fake
-            if self.hparams.gen_model in ["unet", "resnet"]:
-                fake_ = fake + self.generator(fake)
+            if self.hparams.gen_model in ["unet", "refiner"]:
+                fake_ = self.generator(fake)
             else:
-                z = th.normal(0, 1, size=(len(real), self.hparams.latent_dim))
+                z = th.normal(0, 1, size=(
+                    len(real), self.generator.latent_dim), device=self.device)
                 fake_ = self.generator(z)
             fake_pred = self.discriminator(fake_)
             fake_gt = th.zeros_like(fake_pred)
@@ -130,14 +160,14 @@ class GAN(pl.LightningModule):
         # Train generator
         if optimizer_idx == 1:
             l1_loss = 0
-            if self.hparams.gen_model == "unet":
-                delta = self.generator(fake)
-                fake_ = fake + delta
+            if self.hparams.gen_model in ["unet", "refiner"]:
+                fake_ = self.generator(fake)
 
                 # L1-Norm
-                l1_loss = self.hparams.beta * th.sum(th.abs(delta))
+                l1_loss = self.hparams.beta * th.sum(th.abs(fake - fake_))
             else:
-                z = th.normal(0, 1, size=(len(real), self.hparams.latent_dim))
+                z = th.normal(0, 1, size=(
+                    len(real), self.generator.latent_dim), device=self.device)
                 fake_ = self.generator(z)
 
             # Gen Loss
@@ -149,185 +179,35 @@ class GAN(pl.LightningModule):
             result = gen_loss + l1_loss
 
             # Logging
-            self.log("loss/gen", result)
-            if self.hparams.gen_model == "unet":
+            self.log("loss/gen", gen_loss)
+            if self.hparams.gen_model in ["unet", "refiner"]:
                 self.log("loss/gen_l1", l1_loss)
 
         return result
 
+    def on_epoch_end(self):
 
-# import pytorch_lightning as pl
-# import torch as th
-# import torch.nn as nn
+        if (self.current_epoch + 1) % 5 == 0 or self.current_epoch == 0:
+            self.generator.eval()
+            with th.no_grad():
+                act_fake = np.zeros_like(self._act_real)
 
-# from ..layers import *
-# from ..utility import *
-# from .networks import *
+                for i in range(self._n_samples // self._act_samples):
+                    # start and end index
+                    si = i * self._act_samples
+                    ei = min((i+1)*self._act_samples, self._n_samples)
 
-# # -------------------------------------------------------- #
-# #                         GANS                             #
-# # -------------------------------------------------------- #
+                    # images generated by generator
+                    imgs = self.generator(self.gen_input[si:ei])
 
+                    # vgg activations
+                    act = np.squeeze(vgg16_get_activation_maps(
+                        imgs, layer_idx=33, device="cpu", normalize_range=(-1, 1)).numpy())
+                    act_fake[si:ei] = act
+            self.generator.train()
 
-# class GAN(pl.LightningModule):
-#     """My Gan module because I find ``pl_bolts`` implementation confusing.
-#     It is based on https://lightning-bolts.readthedocs.io/en/latest/gans.html#basic-gan
+            # # compute fid
+            fid = fid_score(self._act_real, act_fake, skip_vgg=True)
 
-#     Parameters
-#     ----------
-
-#     """
-
-#     def __init__(self,
-#                  img_dim,
-#                  learning_rate=0.0002,
-#                  latent_dim=100,
-#                  betas=(0.5, 0.999),
-#                  discriminator_params=None,
-#                  generator_params=None,
-#                  use_gp=False,
-#                  alpha=1.0,
-#                  gen_model="basic",
-#                  disc_model="basic"):
-#         super().__init__()
-#         self.save_hyperparameters()
-
-#         self.generator = self.get_generator(generator_params)
-#         self.discriminator = self.get_discriminator(discriminator_params)
-#         self.criterion = self.get_criterion()
-
-#     def get_criterion(self):
-#         return nn.BCELoss()
-
-#     def get_generator(self, generator_params):
-#         generator_params = generator_params if generator_params else {}
-
-#         if self.hparams.gen_model == "resnet":
-#             generator = ResNetGenerator(self.hparams.img_dim,
-#                                         self.hparams.latent_dim, **generator_params)
-#         elif self.hparams.gen_model == "basic":
-#             generator = BasicGenerator(self.hparams.img_dim,
-#                                        self.hparams.latent_dim, **generator_params)
-#         else:
-#             raise NotImplementedError()
-#         generator.apply(weights_init_normal)
-#         return generator
-
-#     def get_discriminator(self, discriminator_params):
-#         discriminator_params = discriminator_params if discriminator_params else {}
-
-#         if self.hparams.disc_model == "basic":
-#             discriminator = BasicDiscriminator(
-#                 self.hparams.img_dim, **discriminator_params)
-#         else:
-#             raise NotImplementedError()
-#         discriminator.apply(weights_init_normal)
-#         return discriminator
-
-#     def configure_optimizers(self):
-#         lr = self.hparams.learning_rate
-#         betas = self.hparams.betas
-#         opt_disc = th.optim.Adam(
-#             self.discriminator.parameters(), lr=lr, betas=betas)
-#         opt_gen = th.optim.Adam(
-#             self.generator.parameters(), lr=lr, betas=betas)
-#         return [opt_disc, opt_gen], []
-
-#     def forward(self, noise: th.Tensor) -> th.Tensor:
-#         """
-#         Generates an image given input noise
-
-#         Example::
-#             noise = th.rand(batch_size, latent_dim)
-#             gan = GAN.load_from_checkpoint(PATH)
-#             img = gan(noise)
-#         """
-#         noise = noise.view(*noise.shape, 1, 1)
-#         return self.generator(noise)
-
-#     def training_step(self, batch, batch_idx, optimizer_idx):
-#         real, _ = batch
-
-#         # Train discriminator
-#         result = None
-#         if optimizer_idx == 0:
-#             result = self.disc_step(real)
-
-#         # Train generator
-#         if optimizer_idx == 1:
-#             result = self.gen_step(real)
-
-#         return result
-
-#     def disc_step(self, real: th.Tensor) -> th.Tensor:
-
-#         # Train with real
-#         real = real.requires_grad_(True)
-#         real_pred = self.discriminator(real)
-#         real_gt = th.ones_like(real_pred)
-#         real_loss = self.criterion(real_pred, real_gt)
-
-#         # Gradient Penalty (R1)
-#         gp_loss = 0
-#         if self.hparams.use_gp:
-#             grad = th.autograd.grad(real_pred, real, grad_outputs=th.ones(real_pred.size()).to(self.device),
-#                                     create_graph=True, retain_graph=True, only_inputs=True)[0]
-#             slopes = th.sqrt(th.sum(th.mul(grad, grad), dim=[1, 2, 3]))
-#             gp_loss = 0.5 * self.hparams.alpha * th.mean(slopes)
-#             # print(gp_loss)
-
-#         # Train with fake
-#         fake_pred = self.get_pred_fake(real)
-#         fake_gt = th.zeros_like(fake_pred)
-#         fake_loss = self.criterion(fake_pred, fake_gt)
-
-#         # Total Loss
-#         disc_loss = real_loss + fake_loss + gp_loss
-
-#         # Logging
-#         self.log("loss/disc", disc_loss)
-#         self.log("loss/disc_real", real_loss)
-#         self.log("loss/disc_fake", fake_loss)
-#         if self.hparams.use_gp:
-#             self.log("loss/disc_gp", gp_loss)
-
-#         return disc_loss
-
-#     def get_pred_fake(self, real: th.Tensor) -> th.Tensor:
-#         noise = th.normal(mean=0.0, std=1.0, size=(
-#             len(real), self.hparams.latent_dim, 1, 1), device=self.device)
-#         fake = self.generator(noise)
-#         return self.discriminator(fake)
-
-#     def gen_step(self, real: th.Tensor) -> th.Tensor:
-
-#         fake_pred = self.get_pred_fake(real)
-#         fake_gt = th.ones_like(fake_pred)
-
-#         gen_loss = self.criterion(fake_pred, fake_gt)
-
-#         self.log("loss/gen", gen_loss)
-#         return gen_loss
-
-#     def on_epoch_end(self):
-
-#         writer = self.logger.experiment
-
-#         self.generator.eval()
-#         self.discriminator.eval()
-
-#         with th.no_grad():
-#             # save weights
-#             for name, params in self.generator.named_parameters():
-#                 if params.grad is not None:
-#                     writer.add_histogram(
-#                         "Generator/" + name, params.detach().cpu().numpy(), self.global_step)
-#             for name, params in self.discriminator.named_parameters():
-#                 if params.grad is not None:
-#                     writer.add_histogram(
-#                         "Discriminator/" + name, params.detach().cpu().numpy(), self.global_step)
-
-#             # save activations
-
-#         self.generator.train()
-#         self.discriminator.train()
+            # logging
+            self.log("loss/fid", fid)
