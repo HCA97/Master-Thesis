39cccf1a5901ea1a2fa1f4e97b36402763cf3fa5
hca <s7hialtu@uni-bonn.de>
2021-06-12 22:44:36 +0200

    training for cube

---
+++ dc_gan_train.sh
@@ -0,0 +1,5 @@
+#block(name=gan_single_hca, threads=4, memory=16000, subtasks=1, gpus=1, hours=4)
+	echo $CUDA_VISIBLE_DEVICES
+    source /home/s7hialtu/anaconda3/etc/profile.d/conda.sh
+	conda activate master-thesis
+	python3 train.py
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 
 from torchsummary import summary
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from torchvision import transforms
 
 from scripts.models import *
@@ -11,50 +11,46 @@ from scripts.dataloader import *
 from scripts.callbacks import *
 
 # POTSDAM CARS
-generator_params = {"n_layers": 4, "init_channels": 512}
-discriminator_params = {"base_channels": 32, "n_layers": 4, "heat_map": True}
+generator_params = {"n_layers": 4, "init_channels": 512, "act": "leakyrelu"}
+discriminator_params = {"base_channels": 32, "n_layers": 4}
 
 img_dim = (3, 32, 64)
 batch_size = 64
-max_epochs = 500
-
-# GP
-use_gp = False
-alpha = 0.1
-
-# Data Aug
-contrast = [1, 1.25]
-hue = [-0.1, 0.1]
-transform = transforms.Compose([transforms.Resize(img_dim[1:]),
-                                transforms.ToTensor(),
-                                transforms.RandomHorizontalFlip(p=0.5),
-                                transforms.RandomVerticalFlip(p=0.5),
-                                transforms.ColorJitter(
-                                    hue=hue, contrast=contrast),
-                                transforms.Normalize([0.5], [0.5])])
+max_epochs = 1000
+interval = 25
 
 data_dir = "/scratch/s7hialtu/potsdam_cars"
-results_dir = "/scratch/s7hialtu/dcgan_heatmap"
+results_dir = "/scratch/s7hialtu/dcgan_bn_momentum"
 
 if not os.path.isdir(data_dir):
     data_dir = "../potsdam_data/potsdam_cars"
     results_dir = "logs"
 
-model = GAN(img_dim, discriminator_params=discriminator_params,
-            generator_params=generator_params, use_gp=use_gp, alpha=alpha)
+
+model = GAN(img_dim, discriminator_params=discriminator_params, fid_interval=interval,
+            generator_params=generator_params, gen_model="basic")
 
 potsdam = PostdamCarsDataModule(
-    data_dir, img_size=img_dim[1:], batch_size=batch_size, transform=transform)
+    data_dir, img_size=img_dim[1:], batch_size=batch_size)
 potsdam.setup()
 
 callbacks = [
     TensorboardGeneratorSampler(
-        epoch_interval=25, num_samples=batch_size, normalize=True),
-    LatentDimInterpolator(interpolate_epoch_interval=25, num_samples=10),
-    ModelCheckpoint(period=25, save_top_k=-1, filename="{epoch}")
+        epoch_interval=interval, num_samples=batch_size, normalize=True),
+    LatentDimInterpolator(
+        interpolate_epoch_interval=interval, num_samples=10),
+    ModelCheckpoint(period=interval, save_top_k=-1, filename="{epoch}"),
+    EarlyStopping(monitor="fid", patience=10*interval, mode="min"),
+    Pix2PixCallback(epoch_interval=interval),
+    ShowWeights(),
+    MyEarlyStopping(300, threshold=5, monitor="fid", mode="min")
 ]
 
 # Apparently Trainer has logger by default
 trainer = pl.Trainer(default_root_dir=results_dir, gpus=1, max_epochs=max_epochs,
                      callbacks=callbacks, progress_bar_refresh_rate=20)
 trainer.fit(model, datamodule=potsdam)
+
+file_name = os.path.basename(__file__)
+copyfile(file_name,
+         os.path.join(results_dir, file_name))
