925b713ef242e934f8ddb46774284b2f097954bb
hca <s7hialtu@uni-bonn.de>
2021-06-15 11:32:41 +0200

    resnet examples

--- experiment/dcgan_experiment_resnet.py
+++ experiment/dcgan_experiment_resnet.py
@@ -11,62 +11,72 @@ from scripts.utility import *
 from scripts.dataloader import *
 from scripts.callbacks import *
 
-# POTSDAM CARS
-
-parammeters = [({"n_layers": 4, "init_channels": 256, "n_blocks": 1}, {
-                "base_channels": 32, "n_layers": 4}),
-               ({"n_layers": 4, "init_channels": 256, "n_blocks": 2}, {
-                "base_channels": 16, "n_layers": 4}),
-               ({"n_layers": 4, "init_channels": 512, "n_blocks": 1}, {
-                "base_channels": 32, "n_layers": 4})]
-use_gp = False
-alpha = 1
+generator_params = [(True, 0.5, {"n_layers": 4, "bn_mode": "default", "init_channels": 512,
+                                 "act": "leakyrelu", "learn_upsample": True, "learn_latent": True, "inject_noise": True}),
+                    (True, 0.5, {"n_layers": 4, "bn_mode": "default", "init_channels": 512,
+                                 "act": "leakyrelu", "learn_upsample": True, "learn_latent": True}),
+                    (True, 0.5, {"n_layers": 4, "bn_mode": "default", "init_channels": 512,
+                                 "act": "leakyrelu", "learn_upsample": True, "inject_noise": True})
+                    (True, 0.5, {"n_layers": 4, "bn_mode": "default",
+                                 "init_channels": 512, "act": "leakyrelu", "learn_upsample": True}),
+                    (True, 0.5, {"n_layers": 4, "bn_mode": "default",
+                                 "init_channels": 512, "act": "leakyrelu"}),
+                    (False, 0, {"n_layers": 4, "bn_mode": "default",
+                                "init_channels": 512, "act": "leakyrelu"})]
+discriminator_params = [{"base_channels": 32, "n_layers": 4, "bn_mode": "default"},
+                        {"base_channels": 32, "n_layers": 4,
+                         "bn_mode": "default", "heat_map": True}
+                        ]
 
 img_dim = (3, 32, 64)
 batch_size = 64
-max_epochs = 1
+max_epochs = 1000
+interval = 25
+
+# DATA AUG
+transform = transforms.Compose([transforms.Resize(img_dim[1:]),
+                                transforms.ToTensor(),
+                                transforms.RandomHorizontalFlip(p=0.5),
+                                transforms.RandomVerticalFlip(p=0.5),
+                                transforms.ColorJitter(hue=[-0.1, 0.1]),
+                                transforms.Normalize([0.5], [0.5])])
+
 data_dir = "/scratch/s7hialtu/potsdam_cars"
-results_dir = "/scratch/s7hialtu/dcgan_bigger/resnet"
+results_dir = "/scratch/s7hialtu/dcgan_resnet"
 
 if not os.path.isdir(data_dir):
     data_dir = "../potsdam_data/potsdam_cars"
     results_dir = "logs"
 
-# data aug
-contrast = [1, 1.25]
-hue = [-0.1, 0.1]
+for discriminator_param in discriminator_params:
+    for use_gp, alpha, generator_param in generator_params:
+        model = GAN(img_dim, discriminator_params=discriminator_param, fid_interval=interval,
+                    generator_params=generator_param, gen_model="resnet", use_gp=use_gp, alpha=alpha)
 
-transform = transforms.Compose([transforms.Resize(img_dim[1:]),
-                                transforms.ToTensor(),
-                                transforms.RandomHorizontalFlip(p=0.5),
-                                transforms.RandomVerticalFlip(p=0.5),
-                                transforms.ColorJitter(
-                                    hue=hue, contrast=contrast),
-                                transforms.Normalize([0.5], [0.5])])
+        potsdam = PostdamCarsDataModule(
+            data_dir, img_size=img_dim[1:], batch_size=batch_size, transform=transform)
+        potsdam.setup()
 
-for generator_param, discriminator_param in parammeters:
-    model = GAN(img_dim, discriminator_params=discriminator_param, gen_model="resnet",
-                generator_params=generator_param, use_gp=use_gp, alpha=alpha, use_lr_scheduler=True)
+        callbacks = [
+            TensorboardGeneratorSampler(
+                epoch_interval=interval, num_samples=batch_size, normalize=True),
+            LatentDimInterpolator(
+                interpolate_epoch_interval=interval, num_samples=10),
+            ModelCheckpoint(period=interval, save_top_k=-
+                            1, filename="{epoch}"),
+            EarlyStopping(monitor="fid", patience=10*interval, mode="min"),
+            Pix2PixCallback(epoch_interval=interval),
+            ShowWeights(epoch_interval=interval)]
+        # MyEarlyStopping(300, threshold=5, monitor="fid", mode="min")
+        # ]
 
-    potsdam = PostdamCarsDataModule(
-        data_dir, img_size=img_dim[1:], batch_size=batch_size, transform=transform)
-    potsdam.setup()
-
-    callbacks = [
-        TensorboardGeneratorSampler(
-            epoch_interval=25, num_samples=batch_size, normalize=True),
-        LatentDimInterpolator(
-            interpolate_epoch_interval=25, num_samples=10),
-        ModelCheckpoint(period=25, save_top_k=-1, filename="{epoch}")
-    ]
-
-    # Apparently Trainer has logger by default
-    trainer = pl.Trainer(default_root_dir=results_dir, gpus=1, max_epochs=max_epochs,
-                         callbacks=callbacks, progress_bar_refresh_rate=20)
-    try:
-        trainer.fit(model, datamodule=potsdam)
-    except KeyboardInterrupt:
-        pass
+        # Apparently Trainer has logger by default
+        trainer = pl.Trainer(default_root_dir=results_dir, gpus=1, max_epochs=max_epochs,
+                             callbacks=callbacks, progress_bar_refresh_rate=20)
+        try:
+            trainer.fit(model, datamodule=potsdam)
+        except KeyboardInterrupt:
+            pass
 
 file_name = os.path.basename(__file__)
 copyfile(os.path.join("experiment", file_name),
