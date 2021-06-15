a66e618f90c66eddc0d09f120f52c71b527fd048
hca <s7hialtu@uni-bonn.de>
2021-06-15 11:33:39 +0200

    hours increased to 24

--- dc_gan_experiments.sh
+++ dc_gan_experiments.sh
@@ -1,4 +1,4 @@
-#block(name=gan_hca, threads=4, memory=16000, subtasks=1, gpus=1, hours=12)
+#block(name=gan_hca, threads=4, memory=16000, subtasks=1, gpus=1, hours=24)
 	echo $CUDA_VISIBLE_DEVICES
     source /home/s7hialtu/anaconda3/etc/profile.d/conda.sh
 	conda activate master-thesis
