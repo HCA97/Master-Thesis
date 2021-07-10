5b1f6025fe6d0f8e124daaa999f90c08e8f6057d
hca <s7hialtu@uni-bonn.de>
2021-07-10 17:23:08 +0200

    generator padding reflect

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import torchvision.utils as ut
 import os
 from shutil import copyfile
 
@@ -13,55 +12,37 @@ from scripts.dataloader import *
 from scripts.callbacks import *
 
 # POTSDAM CARS
-generator_params = {"n_layers": 2,
-                    "base_channels": 32, "padding_mode": "reflect"}
-discriminator_params = {"base_channels": 64, "n_res": 1,
-                        "kernel_size": 3, "padding_mode": "reflect"}
+generator_params = {"n_layers": 4, "init_channels": 512, "padding_mode": "reflect",
+                    "bn_mode": "default", "act": "leakyrelu", "last_layer_kernel_size": 3}
+discriminator_params = {"base_channels": 64, "padding_mode": "reflect",
+                        "n_layers": 4, "bn_mode": "default"}
 
 img_dim = (3, 32, 64)
 batch_size = 64
-max_epochs = 500
+max_epochs = 1000
 interval = 25
 
+data_dir = "/scratch/s7hialtu/potsdam_cars_all"
+results_dir = "/scratch/s7hialtu/dcgan_disc_double_params_padding_reflect"
 
-data_dir1 = "/scratch/s7hialtu/potsdam_cars_all"
-data_dir2 = "/scratch/s7hialtu/artificial_cars"
-results_dir = "/scratch/s7hialtu/munit"
-
-if not os.path.isdir(data_dir1):
-    data_dir1 = "../potsdam_data/potsdam_cars_val"
-    data_dir2 = "../potsdam_data/artifical_cars_small"
+if not os.path.isdir(data_dir):
+    data_dir = "../potsdam_data/potsdam_cars_all"
     results_dir = "logs"
 
 # DATA AUG FOR
-transform1 = transforms.Compose([transforms.Resize(img_dim[1:]),
+transform = transforms.Compose([transforms.Resize(img_dim[1:]),
                                 transforms.ToTensor(),
                                 transforms.RandomHorizontalFlip(p=0.5),
                                 transforms.RandomVerticalFlip(p=0.5),
-                                transforms.ColorJitter(hue=[-0.1, 0.1]),
+                                transforms.ColorJitter(
+                                    hue=[-0.1, 0.1], contrast=[1, 1.25]),
                                 transforms.Normalize([0.5], [0.5])])
-transform2 = transforms.Compose([transforms.Resize(img_dim[1:]),
-                                 transforms.ToTensor(),
-                                 transforms.RandomHorizontalFlip(p=0.5),
-                                 transforms.RandomVerticalFlip(p=0.5),
-                                 transforms.Normalize([0.5], [0.5])])
 
-model = MUNIT(img_dim,
-              discriminator_params=discriminator_params,
-              generator_params=generator_params,
-              gen_init="kaiming",
-              learning_rate_gen=0.0001,
-              learning_rate_disc=0.0004,
-              fid_interval=interval,
-              use_lpips=True)
-# print(model.generator)
+model = GAN(img_dim, discriminator_params=discriminator_params, fid_interval=interval, disc_model="basic",
+            generator_params=generator_params, gen_model="basic")
 
-# print(model.discriminator)
-
-# assert False
 potsdam = PostdamCarsDataModule(
-    data_dir1, img_size=img_dim[1:], batch_size=batch_size,
-    data_dir2=data_dir2, transform=transform1, transform2=transform2)
+    data_dir, img_size=img_dim[1:], batch_size=batch_size, transform=transform)
 potsdam.setup()
 
 callbacks = [
@@ -70,16 +51,15 @@ callbacks = [
     LatentDimInterpolator(
         interpolate_epoch_interval=interval, num_samples=10),
     ModelCheckpoint(period=interval, save_top_k=-1, filename="{epoch}"),
-    # EarlyStopping(monitor="fid", patience=10*interval, mode="min"),
-    MUNITCallback(epoch_interval=interval),
+    EarlyStopping(monitor="fid", patience=10*interval, mode="min"),
     Pix2PixCallback(epoch_interval=interval),
     ShowWeights(),
-    # MyEarlyStopping(300, threshold=5, monitor="fid", mode="min")
+    MyEarlyStopping(300, threshold=5, monitor="fid", mode="min")
 ]
 
 # Apparently Trainer has logger by default
 trainer = pl.Trainer(default_root_dir=results_dir, gpus=1, max_epochs=max_epochs,
-                     callbacks=callbacks, progress_bar_refresh_rate=1)
+                     callbacks=callbacks, progress_bar_refresh_rate=20)
 try:
     trainer.fit(model, datamodule=potsdam)
 except KeyboardInterrupt:
