8341c451fa7b09c65874e2974bbb7280dfb52897
hca <s7hialtu@uni-bonn.de>
2021-10-16 12:34:09 +0200

    clean up is done

--- gan_space.sh
+++
@@ -1,5 +0,0 @@
-
-#block(name=gan_space_hca, threads=6, memory=60000, subtasks=1, gpus=0, hours=8)
-    source /home/s7hialtu/anaconda3/etc/profile.d/conda.sh
-	conda activate master-thesis
-	python3 gan_space_cube.py /scratch/s7hialtu/dcgan_disc_double_params_padding_reflect_lr_scheduler/lightning_logs/version_0/checkpoints/epoch=899.ckpt /scratch/s7hialtu/gan_space/ --n_samples 20000 --layer conv-1 --n_components 16384
